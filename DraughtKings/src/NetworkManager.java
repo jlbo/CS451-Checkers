22e4e02dbf2f1508ac8abccd53379c23e72e114b
Lukasz Woch <lgwspartan@gmail.com>
2017-02-24 19:38:45 -0500

    added blank classes

---
+++ DraughtKings/src/NetworkManager.java
@@ -0,0 +1,24 @@
+
+public class NetworkManager {
+	
+	public void hostGame(){
+		
+	}
+	
+	public void connectToGame(){
+		
+	}
+	
+	public void sendMove(){
+		
+	}
+	
+	public void readMove(){
+		
+	}
+	
+	public void sendAck(){
+		
+	}
+	
+}
