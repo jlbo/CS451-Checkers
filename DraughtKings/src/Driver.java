58f2c95d1f08adbcdde39564950dcd8733ad2dd9
Lukasz Woch <lgwspartan@bitbucket.org>
2017-02-26 20:09:58 -0500

    First pass on driver class

--- DraughtKings/src/Driver.java
+++ DraughtKings/src/Driver.java
@@ -1,6 +1,20 @@
 
 public class Driver {
 
+	boolean isHost;
+	
+	public void clientManager(){
+		this.isHost = false;
+	}
+	
+	public void hostManager(){
+		this.isHost = true;
+	}
+	
+	public void exitGame(){
+		System.exit(0);
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// THIS IS A CHANGE FOR A COMMIT
