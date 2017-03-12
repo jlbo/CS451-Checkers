4eb7a1f8f4d4314a69207d8ed4c77da5f4832a7b
jlbor <jlbor@Sakura>
2017-03-11 22:03:22 -0500

    Worked on Driver

--- DraughtKings/src/Driver.java
+++ DraughtKings/src/Driver.java
@@ -7,53 +7,35 @@ import java.util.HashMap;
 
 public class Driver {
 
-	boolean isHost;
-	
 	public void exitGame(){
 		System.exit(0);
 	}
 	
 	// Use this for hosting a game
-	public NetworkManager hostGame() throws IOException { 
+	public static NetworkManager hostGame() throws IOException { 
 		NetworkManager nm = new NetworkManager();
-		this.isHost = true;
 		return nm;
 	}
 	
 	// Use this when joining a game, Give it the Host's IP address as a String
-	public NetworkManager joinGame(String IP) throws IOException {
+	public static NetworkManager joinGame(String IP) throws IOException {
 		NetworkManager nm = new NetworkManager(IP);
-		this.isHost = false;
 		return nm;
 	}
 	
 	public static void main(String[] args) {
-
-		boolean host = false;
-		if (host) {
-			NetworkManager nm;
-			try {
-				nm = new NetworkManager();
-				nm.sendAck();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			
-		} else {
-			try {
-				NetworkManager nm = new NetworkManager("10.250.39.33");
-				if (nm.readAck())
-					System.out.println("Okie DOkie");
-				else
-					System.out.println("Oops");
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			
-			
+		GameManager gm;
+		NetworkManager nm;
+		
+		if (/*clicks host game*/) {
+			nm = hostGame();
+		} else if (/*clicks join game*/) {
+			String ipaddr = null;//get IP address here
+			nm= joinGame(ipaddr);
 		}
+		
+		gm = new GameManager(nm);
+		gm.start();
 	}
 
 }
